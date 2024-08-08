a8b675fd01da3294b61f49f5042f359000b0d15a
hanizarif <amireshaghi20@gmail.com>
2024-08-08 14:06:44 +0330

    session12

--- index.php
+++ index.php
@@ -9,4 +9,10 @@ $lastname = "zarif"
 $age = 24
 //float
 $height = 171
+$Info = array("fnam"=>"mahdi" , "iname" =>"kardani" ,"age" => 20)
+$Info = array("fnam"=>"aref" , "iname" =>"askari" ,"age" => 16)
+$Info = array("fnam"=>"mahya" , "iname" =>"hamidi" ,"age" => 16)
+$Info = array("fnam"=>"maede" , "iname" =>"goharshahi" ,"age" => 20)
+$Info = array("fnam"=>"reyhaneh" , "iname" =>"soltani" ,"age" =>16)
+$Info = array("fnam"=>"zeinb" , "iname" =>"gharib" ,"age" =>16)
 ?>
